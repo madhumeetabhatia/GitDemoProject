e6325c1317fd34512b33ae78a58df956cc739d99
Madhumeeta <Madhumeeta@Madhumeeta-PC>
2020-07-30 20:56:40 +0530

    Add new check

---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+     String  hungry="yes";
+      if(hungry=="yes" ||hungry=="Y")
+      
+    	  System.out.println("Eat something..");
+    	  
+      
+      else
+    	  System.out.println("Do work...");
+	    
+		
+
+	}
+
+}
